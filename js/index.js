060ce127af42ffbf5447c5377da83457ca10328d
HectorGallego96 <gallegohector96@gmail.com>
2022-07-09 22:50:58 -0500

    se agregar tabla y se realizan ajustes en las lista de pedidos

--- js/index.js
+++ js/index.js
@@ -6,13 +6,26 @@
 
 // declaracion de variables
 let btnAgregar = document.querySelector("#btnAgregar");
-let btnPasar = document.querySelector("#pasar");
+let btnEjecucion = document.querySelector("#ejecucion");
 let filaIngresados = document.querySelector(".pedidos-ingresados");
 let combo = document.querySelector("#comidas");
-let x = 0;
-
 let filaProcesamiento = document.querySelector("#filaProcesamiento");
 let filaEntregado = document.querySelector("#filaEntregado");
+let tabla = document.querySelector(".table")
+let tBody = tabla.querySelector(".t-body")
+let mediaRespuesta = document.querySelector(".media-respuesta")
+let mediaEspera = document.querySelector(".media-espera")
+let divQuantum = document.querySelector(".quantum");
+
+
+// contador de numero de orden
+let contNumOrden = 0;
+
+let tiemposEspera
+let tiemposRespuesta
+
+
+// arrays para guardar el orden en que se preparan y el oreden en que quedan preparados
 let listaPreparacion = [];
 let listaPreparados = [];
 
@@ -21,7 +34,7 @@ let numeroProceso;
 let rafagas = [];
 let quantum = 2;
 
-// procesos de ejecucion
+// contadores de  procesos de ejecucion
 let cont = 0;
 let cont2 = 0;
 
@@ -35,9 +48,10 @@ let tiempoPreparacionComidas = {
     Pizza: 7
 }
 
+// clase Pedido
 class Pedido {
 
-    constructor(numeroOrden, pedido, tiempoPreparacion) {
+    constructor(numeroOrden, pedido) {
         this.numeroOrden = numeroOrden;
         this.pedido = pedido;
         this.tiempoPreparacion = tiempoPreparacionComidas[pedido];
@@ -127,6 +141,7 @@ const calcularTiempoDeEspera = (procesos, numeroProcesos, rafagas, tiemposEspera
                     listaPreparacion.push(procesos[i]);
 
 
+
                 }
 
                 // Si el tiempo de ráfaga es menor o igual que el cuanto. Último ciclo para este proceso
@@ -153,7 +168,7 @@ const calcularTiempoDeEspera = (procesos, numeroProcesos, rafagas, tiemposEspera
 }
 
 // Función para calcular el tiempo de vuelta
-const encontrarTiempoDeRespuesta = (procesos, n, rafagas, tiemposEspera, tiempoRespuesta) => {
+const calcularTiempoDeRespuesta = (procesos, n, rafagas, tiemposEspera, tiempoRespuesta) => {
     // calcular el tiempo de respuesta sumando rafagas[i] + tiemposEspera[i]
     for (let i = 0; i < n; i++)
         tiempoRespuesta[i] = rafagas[i] + tiemposEspera[i];
@@ -162,12 +177,12 @@ const encontrarTiempoDeRespuesta = (procesos, n, rafagas, tiemposEspera, tiempoR
 // Función para calcular el tiempo promedio
 const calcularTiempoPromedio = (procesos, numeroProcesos, rafagas, quantum) => {
 
-    let tiemposEspera = new Array(numeroProcesos).fill(0);
-    let tiemposRespuesta = new Array(numeroProcesos).fill(0);
+    tiemposEspera = new Array(numeroProcesos).fill(0);
+    tiemposRespuesta = new Array(numeroProcesos).fill(0);
 
 
     calcularTiempoDeEspera(procesos, numeroProcesos, rafagas, tiemposEspera, quantum);
-    encontrarTiempoDeRespuesta(procesos, numeroProcesos, rafagas, tiemposEspera, tiemposRespuesta);
+    calcularTiempoDeRespuesta(procesos, numeroProcesos, rafagas, tiemposEspera, tiemposRespuesta);
 
 
 }
@@ -179,6 +194,12 @@ const delay = millis =>
         setTimeout(resolve, millis);
     });
 
+/* funcion asincrona para simular pausa en proceso de ejecución
+ fn = recive una funcion;
+ repet = las veces que se quiere repetir esa funcion
+ millis = tiempo de espera entre la llamada de cada funcion
+ ...args = lista de argumentos para ejecutar la funcion (opcional)
+*/
 const interLock = async (fn, repeat, millis, ...args) => {
     for (let i = 0; i < repeat - 1; ++i) {
         fn(...args);
@@ -187,38 +208,97 @@ const interLock = async (fn, repeat, millis, ...args) => {
     fn(...args);
 }
 
-const obj = {
-    play: function () {
+// objeto
+const funcionesProcesamiento = {
 
+    // funcion para simular el orden en que se ejecutan los procesos
+    // y representarlos graficamente
+    simulacionProcesamiento: function () {
 
         let div = document.createElement("div");
-        div.textContent = "Numero Orden: " + listaPreparacion[cont] + " Pedido: "+filaPedidos.items[listaPreparacion[cont]].pedido;
+        div.textContent = "Numero Orden: " + listaPreparacion[cont] + "- Pedido: " + filaPedidos.items[listaPreparacion[cont]].pedido + " - Ejecutandose...";
         div.classList.add("item-proce");
         filaProcesamiento.appendChild(div);
         cont++
 
         if (cont === listaPreparacion.length) {
-            interLock(() => obj.listos(), listaPreparados.length, 3000);
+            interLock(() => funcionesProcesamiento.simulacionProcesados(), listaPreparados.length, 2000);
         }
 
     },
-    listos: function () {
+    // funcion para simular el orden en que terminan de ejecutarse los procesos
+    // y representarlos graficamente
+    simulacionProcesados: function () {
 
         let div = document.createElement("div");
         div.textContent = listaPreparados[cont2];
+        div.textContent = "Numero Orden: " + listaPreparados[cont2] + "- Pedido: " + filaPedidos.items[listaPreparados[cont2]].pedido + " - Ejecutado ";
         div.classList.add("item-ent");
         filaEntregado.appendChild(div);
         cont2++;
 
+        if (listaPreparados.length === cont2) {
+            interLock(() => funcionesProcesamiento.imprimiTabla(), 1, 2000);
+        }
+
     },
+
+    imprimiTabla: function () {
+        let contTiempoEspera = 0
+        let contTiempoRespuesta = 0
+
+        for (let i = 0; i < tiemposEspera.length; i++) {
+
+            let tr = document.createElement("tr");
+            let tdProceso = document.createElement("td");
+            let tdRafagas = document.createElement("td");
+            let tdTiempoespera = document.createElement("td");
+            let tdTiempoRespuesta = document.createElement("td");
+
+            console.log(i)
+            tdProceso.textContent = i;
+            tdRafagas.textContent = filaPedidos.items[i].tiempoPreparacion;
+            tdTiempoespera.textContent = tiemposEspera[i];
+            tdTiempoRespuesta.textContent = tiemposRespuesta[i];
+
+
+            tr.appendChild(tdProceso)
+            tr.appendChild(tdRafagas)
+            tr.appendChild(tdTiempoespera)
+            tr.appendChild(tdTiempoRespuesta)
+
+            tBody.appendChild(tr)
+            console.log(tBody)
+
+            contTiempoEspera += tiemposEspera[i];
+            contTiempoRespuesta += tiemposRespuesta[i];
+
+        }
+
+
+
+        mediaRespuesta.style.padding= "0.5em";
+        mediaEspera.style.padding= "0.5em";
+        divQuantum.style.padding = "0.5em";
+
+        divQuantum.textContent = "Quantum: "+quantum;
+        mediaRespuesta.textContent = ("Tiempo medio de Respuesta: " + (contTiempoRespuesta / tiemposRespuesta.length).toFixed(2));
+        mediaEspera.textContent = ("Tiempo medio de Espera: " + (contTiempoEspera / tiemposEspera.length).toFixed(2));
+    }
 
 }
 
-btnPasar.addEventListener("click", iniciarProcesamientoRR);
 
+// funcion simulacion procesamiento RR
 function iniciarProcesamientoRR() {
 
+
     numeroProceso = filaPedidos.getSize();
+
+    if(numeroProceso <= 0){
+        alert("La Cola de Pedidos no puede estar vacia")
+        return
+    }
     for (let i = 0; i < numeroProceso; i++) {
 
         procesos.push(filaPedidos.items[i].numeroOrden);
@@ -227,17 +307,20 @@ function iniciarProcesamientoRR() {
 
     calcularTiempoPromedio(procesos, numeroProceso, rafagas, quantum);
     // promesa para encadenar los algoritmos
-    interLock(() => obj.play(), listaPreparacion.length, 3000);
+    interLock(() => funcionesProcesamiento.simulacionProcesamiento(), listaPreparacion.length, 2000);
 
 }
 
-// algoritmo fila fifo
 
+// evento para iniciar simulacion de operacion RR
+btnEjecucion.addEventListener("click", iniciarProcesamientoRR);
+
+// evento para agregar elementos a la cola
 btnAgregar.addEventListener("click", function (event) {
     event.preventDefault();
 
     const comida = combo.options[combo.selectedIndex].text;
-    const pedido = new Pedido(x++, comida, 5);
+    const pedido = new Pedido(contNumOrden++, comida, 5);
 
     filaPedidos.push(pedido);
 
@@ -251,13 +334,10 @@ btnAgregar.addEventListener("click", function (event) {
     let p = document.createElement("p");
     let ul = document.createElement("ul");
     let li = document.createElement("li");
-
-    let ul2 = document.createElement("ul");
     let li2 = document.createElement("li");
 
 
     for (let i = 0; i < filaPedidos.getSize(); i++){
-
 
         img.classList.add("card-img-top");
         cardBody.classList.add("card-body");
@@ -280,19 +360,16 @@ btnAgregar.addEventListener("click", function (event) {
         ul.appendChild(li);
         ul.appendChild(li2);
 
-
-
         cardBody.appendChild(h5);
         cardBody.appendChild(p);
-
 
         card.appendChild(img);
         card.appendChild(cardBody);
         card.appendChild(ul);
 
-
         filaIngresados.appendChild(card);
     }
 
 });
+
 
